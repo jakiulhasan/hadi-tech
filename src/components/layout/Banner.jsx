9ee0c099cd1642a47568a9c11aab73dcbc126947
Md Jakiul Hasan <mdjakiulhasan17@gmail.com>
2026-01-18 01:56:46 +0600

    banner responsive

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -1,4 +1,4 @@
-"use client"; // Required for Swiper as it uses browser APIs
+"use client";
 
 import React from "react";
 import Image from "next/image";
@@ -14,9 +14,10 @@ const Banner = () => {
   const sliderImages = ["/1.jpg", "/2.jpg", "/3.jpg"];
 
   return (
-    <div className="flex gap-4 max-w-7xl mx-auto px-4 h-125 my-10">
+    <div className="flex flex-col md:flex-row gap-4 max-w-7xl mx-auto px-4 my-6 md:my-10 h-auto md:h-[500px]">
       {/* Left Side: Swiper Slider */}
-      <div className="w-3/4 h-full overflow-hidden rounded-lg">
+      {/* Mobile: 300px height | Desktop: 3/4 width, full height */}
+      <div className="w-full md:w-3/4 h-[300px] md:h-full overflow-hidden rounded-lg">
         <Swiper
           spaceBetween={10}
           centeredSlides={true}
@@ -37,7 +38,7 @@ const Banner = () => {
                   alt={`Slide ${index + 1}`}
                   fill
                   className="object-cover"
-                  priority={index === 0} // Load first image faster
+                  priority={index === 0}
                 />
               </div>
             </SwiperSlide>
@@ -46,11 +47,12 @@ const Banner = () => {
       </div>
 
       {/* Right Side: Static Images */}
-      <div className="w-1/4 flex flex-col gap-4 h-full">
-        <div className="relative w-full h-1/2 overflow-hidden rounded-lg bg-gray-200">
+      {/* Mobile: Row of two images | Desktop: Column of two images */}
+      <div className="w-full md:w-1/4 flex flex-row md:flex-col gap-4 h-[150px] md:h-full">
+        <div className="relative w-1/2 md:w-full h-full md:h-1/2 overflow-hidden rounded-lg bg-gray-200">
           <Image src="/4.jpg" alt="Promo 1" fill className="object-cover" />
         </div>
-        <div className="relative w-full h-1/2 overflow-hidden rounded-lg bg-gray-200">
+        <div className="relative w-1/2 md:w-full h-full md:h-1/2 overflow-hidden rounded-lg bg-gray-200">
           <Image src="/5.jpg" alt="Promo 2" fill className="object-cover" />
         </div>
       </div>
